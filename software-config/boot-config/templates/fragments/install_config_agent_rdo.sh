ded2a766e80067f44e70f3ddf687d631eea36b24
Florin Stingaciu <stingaci@mirantisits-MacBook-Pro-2.local>
2016-12-21 16:58:06 -0800

    Added support for HTTP_PROXY and HTTPS_PROXY in centos/rdo install scripts

--- software-config/boot-config/templates/fragments/install_config_agent_rdo.sh
+++ software-config/boot-config/templates/fragments/install_config_agent_rdo.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 set -eux
 
+http_proxy=$http_proxy_val
+https_proxy=$https_proxy_val
+
+if [ "$http_proxy" != "None" ]
+then
+	export HTTP_PROXY=$http_proxy
+	echo "Setting http_proxy var: $HTTP_PROXY"
+fi
+
+if [ "$https_proxy" != "None" ]
+then
+	export HTTPS_PROXY=$https_proxy
+	echo "Setting https_proxy var: $HTTPS_PROXY"
+fi
+
 yum -y install https://www.rdoproject.org/repos/rdo-release.rpm
 yum -y install python-zaqarclient python-oslo-log python-psutil os-collect-config os-apply-config os-refresh-config dib-utils
